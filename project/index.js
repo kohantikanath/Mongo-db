491977cbad978b6104e0a8387c728fc42d47e82f
Kohantika Nath <kohan.nath@gail.com>
2024-07-10 01:15:03 +0530

    1st commit

--- project/index.js
+++ project/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(express.json());
 
@@ -50,25 +50,27 @@ app.post("/api/products", async (req, res) => {
   return res.status(201).json({ message: "Product Created" });
 });
 
-app.get('/api/products' , async(req, res) =>{
-    const allProducts = await productModel.find({})
+app.get("/api/products", async (req, res) => {
+  const allProducts = await productModel.find({});
 
-    return res.json(allProducts)
-})
+  return res.json(allProducts);
+});
 
 //Get product by id
-app.get('/api/products/:id', async(req, res) =>{
-    const product = await productModel.findById(req.params.id)
+app.get("/api/products/:id", async (req, res) => {
+  const product = await productModel.findById(req.params.id);
 
-    return res.json(product)
-})
+  return res.json(product);
+});
 
 //Update product
-app.put('/api/products/:id', async(req, res)=>{
-    const updatedProduct = await productModel.findByIdAndUpdate(req.params.id, res.body)
-    return res.json(updatedProduct)
-})
-
+app.put("/api/products/:id", async (req, res) => {
+  const updatedProduct = await productModel.findByIdAndUpdate(
+    req.params.id,
+    res.body
+  );
+  return res.json(updatedProduct);
+});
 
 //Delete a Resource
 app.delete("/api/products/:id", async (req, res) => {
@@ -78,7 +80,6 @@ app.delete("/api/products/:id", async (req, res) => {
   );
   return res.json(deletedProduct);
 });
-
 
 app.listen(8000, () => {
   console.log("Server started at port 8000");
